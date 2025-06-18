fd9442f9794c0451d11c7b5755dea23751f8c30b
ikarizxc <ikarizxc@yandex.ru>
2025-06-18 16:46:27 +0700

    application preprocessor changes

--- src/preprocess/application_preprocessor.py
+++ src/preprocess/application_preprocessor.py
@@ -19,7 +19,20 @@ class ApplicationPreprocessor(BasePreprocessor):
         self._application_test = pd.read_csv('data/application_test.csv', nrows=n)
         self._test_ids = self._application_test['SK_ID_CURR']
         
-        self._base_preprocess()
+        self._ignore_features = []
+        
+        self._set_data()
+        
+    def merge_data(self, data: pd.DataFrame, on: str='SK_ID_CURR', how: str='left'):
+        """
+        Мёрджит данные по ключу в application данные.
+        
+        Args:
+            data (DataFrame): Данные, которы смёрджатся.
+            on (string): Колонка по которой мерджится.
+            how (string): {'left', 'right', 'outer', 'inner', 'cross'} Тип мерджа.
+        """
+        self._X.merge(data, on=on, how=how)
         
     def get_prepared_data(self) -> tuple[pd.DataFrame, pd.Series, pd.DataFrame]:
         """
@@ -29,12 +42,17 @@ class ApplicationPreprocessor(BasePreprocessor):
             Tuple[pd.DataFrame, pd.Series, pd.DataFrame]:
                 X_train, y_train, X_test.
         """
-        self._preprocessed = self._preprocess(self._preprocessed)
+        self._X.loc[self._X['DAYS_EMPLOYED'] > 0, 'DAYS_EMPLOYED'] = np.nan
+        self._X = self._delete_duplicates(self._X)
+        self._X[self._get_features_to_preprocess()] = self._cap_outliers(self._X[self._get_features_to_preprocess()])
         
-        X = pd.concat([self._other, self._preprocessed], axis=1)
-                
-        X_train = X[X['is_train'] == 1].drop(columns=['is_train'])
-        X_test = X[X['is_train'] == 0].drop(columns=['is_train'])
+        subset = self._X[self._get_features_to_preprocess()]
+        processed = self._dummy_encode_categorical_features(subset)
+        self._X.drop(columns=subset.columns, inplace=True)
+        self._X = pd.concat([self._X, processed], axis=1)
+        
+        X_train = self._X[self._X['is_train'] == 1].drop(columns=['is_train'])
+        X_test = self._X[self._X['is_train'] == 0].drop(columns=['is_train'])
         
         return X_train, self._y, X_test
         
@@ -43,36 +61,36 @@ class ApplicationPreprocessor(BasePreprocessor):
         Добавляет бинарный признак SINGLE_FAMILY_STATUS
         (вдова или не замужем - 1).
         """
-        self._preprocessed['SINGLE_FAMILY_STATUS'] = (
-            (self._preprocessed['NAME_FAMILY_STATUS'] == 'Widow') |
-            (self._preprocessed['NAME_FAMILY_STATUS'] == 'Single / not married')
+        self._X['SINGLE_FAMILY_STATUS'] = (
+            (self._X['NAME_FAMILY_STATUS'] == 'Widow') |
+            (self._X['NAME_FAMILY_STATUS'] == 'Single / not married')
         ).astype('int')
         
     def add_contacts_number(self) -> None:
         """
         Добавляет признак суммарного числа контактов.
         """
-        self._preprocessed['CONTACTS_NUMBER'] = \
-            self._preprocessed['FLAG_MOBIL'] + \
-            self._preprocessed['FLAG_WORK_PHONE'] + \
-            self._preprocessed['FLAG_PHONE'] + \
-            self._preprocessed['FLAG_EMAIL']
+        self._X['CONTACTS_NUMBER'] = \
+            self._X['FLAG_MOBIL'] + \
+            self._X['FLAG_WORK_PHONE'] + \
+            self._X['FLAG_PHONE'] + \
+            self._X['FLAG_EMAIL']
         
     def add_bad_car(self) -> None:
         """
         Добавляет признак: клиент без машины или со старой (>10 лет).
         """
-        self._preprocessed['BAD_CAR'] = (
-            (self._preprocessed['FLAG_OWN_CAR'] == 0) |
-            (self._preprocessed['OWN_CAR_AGE'] > 10) 
+        self._X['BAD_CAR'] = (
+            (self._X['FLAG_OWN_CAR'] == 0) |
+            (self._X['OWN_CAR_AGE'] > 10) 
         ).astype('int')
         
     def add_working_hours(self) -> None:
         """
         Добавляет признак: заявка была совершена в рабочие часы (8–18) или нет.
         """
-        self._preprocessed['IS_HOURS_WORKING'] = (
-            self._preprocessed['HOUR_APPR_PROCESS_START']
+        self._X['IS_HOURS_WORKING'] = (
+            self._X['HOUR_APPR_PROCESS_START']
                 .between(8, 18)
                 .astype(int)
         )
@@ -81,41 +99,41 @@ class ApplicationPreprocessor(BasePreprocessor):
         """
         Добавляет признак: наличие связей с дефолтом (>0).
         """
-        self._preprocessed['HAS_BAD_PERS_IN_SOC_CIRCLE'] = (self._preprocessed['DEF_30_CNT_SOCIAL_CIRCLE'] > 0).astype('int')
+        self._X['HAS_BAD_PERS_IN_SOC_CIRCLE'] = (self._X['DEF_30_CNT_SOCIAL_CIRCLE'] > 0).astype('int')
         
     def add_credit_features(self) -> None:
         """
         Добавляет признаки: соотношений кредитных величин.
         """
-        self._preprocessed['CREDIT_INCOME_RATIO'] = self._preprocessed['AMT_CREDIT'] / self._preprocessed['AMT_INCOME_TOTAL']
-        self._preprocessed['ANNUITY_CREDIT_RATIO'] = self._preprocessed['AMT_ANNUITY'] / self._preprocessed['AMT_CREDIT']
-        self._preprocessed['CREDIT_MONTHS'] = self._preprocessed['AMT_CREDIT'] / self._preprocessed['AMT_ANNUITY']
-        self._preprocessed['INITIAL_CREDIT_PAY'] = self._preprocessed['AMT_GOODS_PRICE'] - self._preprocessed['AMT_CREDIT']
+        self._X['CREDIT_INCOME_RATIO'] = self._X['AMT_CREDIT'] / self._X['AMT_INCOME_TOTAL']
+        self._X['ANNUITY_CREDIT_RATIO'] = self._X['AMT_ANNUITY'] / self._X['AMT_CREDIT']
+        self._X['CREDIT_MONTHS'] = self._X['AMT_CREDIT'] / self._X['AMT_ANNUITY']
+        self._X['INITIAL_CREDIT_PAY'] = self._X['AMT_GOODS_PRICE'] - self._X['AMT_CREDIT']
         
     def add_documents_count(self) -> None:
         """
         Добавляет признак: количество поданных документов FLAG_DOCUMENT_*.
         """
-        self._preprocessed['DOCUMENTS_COUNT'] = self._preprocessed[[col for col in self._preprocessed.columns.values if col.startswith('FLAG_DOCUMENT')]].sum(axis=1)
+        self._X['DOCUMENTS_COUNT'] = self._X[[col for col in self._X.columns.values if col.startswith('FLAG_DOCUMENT')]].sum(axis=1)
         
     def add_agg_ext_sources(self) -> None:
         """
         Добавляет признаки: агрегация EXT_SOURCE_{1,2,3}: min/max/mean/std/ratio/weighted.
         """
-        self._preprocessed["EXT_SOURCE_MIN"] = self._preprocessed[['EXT_SOURCE_1', 'EXT_SOURCE_2', 'EXT_SOURCE_3']].min(axis=1)
-        self._preprocessed["EXT_SOURCE_MAX"] = self._preprocessed[['EXT_SOURCE_1', 'EXT_SOURCE_2', 'EXT_SOURCE_3']].max(axis=1)
-        self._preprocessed["EXT_SOURCE_MEAN"] = self._preprocessed[['EXT_SOURCE_1', 'EXT_SOURCE_2', 'EXT_SOURCE_3']].mean(axis=1)
-        self._preprocessed["EXT_SOURCE_STD"] = self._preprocessed[['EXT_SOURCE_1', 'EXT_SOURCE_2', 'EXT_SOURCE_3']].std(axis=1)
-        self._preprocessed["EXT_SOURCE_MIN_MAX_DIV"] = self._preprocessed['EXT_SOURCE_MIN'] / self._preprocessed['EXT_SOURCE_MAX']
-        self._preprocessed["EXT_SOURCE_WEIGHTED"] = (self._preprocessed['EXT_SOURCE_1'] + 5 * self._preprocessed['EXT_SOURCE_2'] + 3 * self._preprocessed['EXT_SOURCE_3']) / 3
+        self._X["EXT_SOURCE_MIN"] = self._X[['EXT_SOURCE_1', 'EXT_SOURCE_2', 'EXT_SOURCE_3']].min(axis=1)
+        self._X["EXT_SOURCE_MAX"] = self._X[['EXT_SOURCE_1', 'EXT_SOURCE_2', 'EXT_SOURCE_3']].max(axis=1)
+        self._X["EXT_SOURCE_MEAN"] = self._X[['EXT_SOURCE_1', 'EXT_SOURCE_2', 'EXT_SOURCE_3']].mean(axis=1)
+        self._X["EXT_SOURCE_STD"] = self._X[['EXT_SOURCE_1', 'EXT_SOURCE_2', 'EXT_SOURCE_3']].std(axis=1)
+        self._X["EXT_SOURCE_MIN_MAX_DIV"] = self._X['EXT_SOURCE_MIN'] / self._X['EXT_SOURCE_MAX']
+        self._X["EXT_SOURCE_WEIGHTED"] = (self._X['EXT_SOURCE_1'] + 5 * self._X['EXT_SOURCE_2'] + 3 * self._X['EXT_SOURCE_3']) / 3
         
     def add_days_percents_features(self) -> None:
         """
         Добавляет признаки соотношения дней: Employment/Birth, Registration/Birth, Publish/Birth.
         """
-        self._preprocessed['DAYS_EMP_BIRTH_PERCENT'] = self._preprocessed['DAYS_EMPLOYED'] / self._preprocessed['DAYS_BIRTH']
-        self._preprocessed['DAYS_REG_BIRTH_PERCENT'] = self._preprocessed['DAYS_REGISTRATION'] / self._preprocessed['DAYS_BIRTH']
-        self._preprocessed['DAYS_PUB_BIRTH_PERCENT'] = self._preprocessed['DAYS_ID_PUBLISH'] / self._preprocessed['DAYS_BIRTH']
+        self._X['DAYS_EMP_BIRTH_PERCENT'] = self._X['DAYS_EMPLOYED'] / self._X['DAYS_BIRTH']
+        self._X['DAYS_REG_BIRTH_PERCENT'] = self._X['DAYS_REGISTRATION'] / self._X['DAYS_BIRTH']
+        self._X['DAYS_PUB_BIRTH_PERCENT'] = self._X['DAYS_ID_PUBLISH'] / self._X['DAYS_BIRTH']
         
     def delete_high_correlation_features(self, threshold: float=0.85) -> None:
         """
@@ -124,19 +142,20 @@ class ApplicationPreprocessor(BasePreprocessor):
         Args:
             threshold (Optional[float]): Порог корреляции
         """
-        self._preprocessed = self._delete_high_correlation_features(self._preprocessed, threshold)
+        self._X[self._get_features_to_preprocess()] = self._delete_high_correlation_features(self._X[self._get_features_to_preprocess()], threshold)
         
-    def _base_preprocess(self) -> None:
+    def _get_features_to_preprocess(self):
+        return [col for col in self._X.columns.values if col not in self._ignore_features]
+        
+    def _set_data(self) -> None:
         """
         Объединяет train/test. Делит выборку на выборку для дальнейшего препроцесса и то что не нужно препроцессить
         """
         X, y = self._concat_train_test()
         
-        ignore_features = ['is_train', 'SK_ID_CURR']
-        relevant_features = [col for col in X.columns if col not in ignore_features]
+        self._ignore_features = ['is_train', 'SK_ID_CURR']
         
-        self._preprocessed = X[relevant_features]
-        self._other = X[ignore_features]
+        self._X = X
         self._y = y 
     
     def _concat_train_test(self) -> tuple[pd.DataFrame, pd.Series]:
@@ -155,19 +174,3 @@ class ApplicationPreprocessor(BasePreprocessor):
         data = pd.concat([self._application_train, self._application_test])
         
         return data, y
-    
-    def _preprocess(self, data: pd.DataFrame) -> pd.DataFrame:
-        """
-        Выполняет последовательный вызов методов препроцессинга: удаление дубликатов, ограничение выбросов, дамми кодирование категориальных признаков.
-
-        Args:
-            data (pd.DataFrame): Объединённый DataFrame.
-
-        Returns:
-            pd.DataFrame: Обработанные данные.
-        """
-        data.loc[data['DAYS_EMPLOYED'] > 0, 'DAYS_EMPLOYED'] = np.nan
-        data = self._delete_duplicates(data)
-        data = self._cap_outliers(data)
-        data = self._dummy_encode_categorical_features(data)
-        return data
